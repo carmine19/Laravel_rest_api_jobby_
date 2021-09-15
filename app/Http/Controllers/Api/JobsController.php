fbc1c23012b168dd2435faed5acc57e17439c6bf
carmine <carminepepiciello@gmail.com>
2021-09-16 00:10:01 +0200

    joblist component

--- app/Http/Controllers/Api/JobsController.php
+++ app/Http/Controllers/Api/JobsController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Job;
 use Illuminate\Http\Request;
-
-
+use phpDocumentor\Reflection\DocBlock\Tags\Author;
 
 
 class JobsController extends Controller
 {
     public function index() {
 
-        $data = Job::paginate(5);
+        $jobs = Job::paginate(5);
 
-        return response()->json($data);
+
+        return response()->json([
+            'success'=> true,
+            'jobs' => $jobs
+        ]);
     }
 }
---
+++ resources/js/Pages/JobList.vue
@@ -0,0 +1,148 @@
+<template>
+<div>
+    joblist
+
+     <div class="container mx-auto px-2 sm:px-10 max-w-3xl">
+            <div class="py-8">
+                <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                    <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                        <table class="min-w-full leading-normal">
+                            <thead>
+                            <tr>
+                                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                    titolo
+                                </th>
+                                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                    descrizione
+                                </th>
+                                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                    data di inizio attività
+                                </th>
+                                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                    durata
+                                </th>
+                                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                    compenso orario
+                                </th>
+                                <th scope="col" class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                    autore
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+
+                            <tr v-for="job in jobList" :key="job.id">
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0">
+                                            <a href="#" class="block relative">
+
+                                            </a>
+                                        </div>
+                                        <div class="ml-3">
+                                            <p class="text-gray-900 whitespace-no-wrap">
+                                               {{ job.title }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        {{ job.description }}
+                                    </p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        {{ job.start_activity_date }}
+                                    </p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                    <span aria-hidden="true" class="absolute inset-0 bg-green-200 opacity-50 rounded-full">
+                                    </span>
+                                    <span class="relative">
+                                        {{ job.duration_activity }}
+                                    </span>
+                                </span>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                                        {{ job.price }}
+                                    </a>
+                                </td>
+
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                                        {{ job.author_id }}
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody>
+
+
+                        </table>
+                        <div class="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
+                            <div class="flex items-center">
+                                <button type="button" class="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100">
+                                    <svg width="9" fill="currentColor" height="8" class="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
+                                        </path>
+                                    </svg>
+                                </button>
+                                <button type="button" class="w-full px-4 py-2 border-t border-b text-base text-indigo-500 bg-white hover:bg-gray-100 ">
+                                    1
+                                </button>
+                                <button type="button" class="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
+                                    2
+                                </button>
+                                <button type="button" class="w-full px-4 py-2 border-t border-b text-base text-gray-600 bg-white hover:bg-gray-100">
+                                    3
+                                </button>
+                                <button type="button" class="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
+                                    4
+                                </button>
+                                <button type="button" class="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100">
+                                    <svg width="9" fill="currentColor" height="8" class="" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
+                                        </path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "JobList",
+
+    data() {
+            return {
+              jobList: {},
+            }
+        },
+        methods: {
+            getJob(){
+                axios.get('/api/jobs')
+                     .then((response)=>{
+                         console.log(response.data.jobs)
+                         this.jobList = response.data.jobs.data
+                     })
+            }
+        },
+        created() {
+            this.getJob()
+            console.log(this.jobList)
+        }
+}
+</script>
+
+<style scoped>
+
+</style>
